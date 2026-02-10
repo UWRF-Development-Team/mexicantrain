d04158f0aa6714bf947dd12329ee7d3c27e7eb8c
andro <90076821+iiandromedaa@users.noreply.github.com>
2026-02-10 17:05:30 -0600

    init

---
+++ src/main/java/org/acm/uwrf/Main.java
@@ -0,0 +1,16 @@
+package org.acm.uwrf;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        // uncomment this to launch the new libgdx window, it just draws a circle to the center of the screen
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new MexicanTrain(), config);
+
+        System.out.println("hi world hello world hii!!!!!");
+    }
+
+}
---
+++ src/main/java/org/acm/uwrf/MexicanTrain.java
@@ -0,0 +1,23 @@
+package org.acm.uwrf;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+public class MexicanTrain extends ApplicationAdapter {
+
+    ShapeRenderer shape;
+
+    @Override
+    public void create() {
+        shape = new ShapeRenderer();
+    }
+
+    @Override
+    public void render() {
+        shape.begin(ShapeRenderer.ShapeType.Filled);
+        shape.circle(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2, 50);
+        shape.end();
+    }
+
+}
